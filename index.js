391a9029c8191f616a250823de1c2ac92da9d681
Konstantin Krumin <konstantin.krumin@gmail.com>
2021-11-23 02:40:20 +0300

    created and set up database models

--- index.js
+++ index.js
@@ -4,15 +4,31 @@ const mongoose = require('mongoose');
 
 require('dotenv').config();
 
+const Post = require('./models/Post');
+
 const typeDefs = gql`
+  type Post {
+    id: ID!
+    body: String!
+    createdAt: String!
+    username: String!
+  }
+
   type Query {
-    sayHi: String!
+    getPosts: [Post]
   }
 `;
 
 const resolvers = {
   Query: {
-    sayHi: () => 'Hello world!!',
+    async getPosts() {
+      try {
+        const posts = await Post.find();
+        return posts;
+      } catch (err) {
+        throw new Error(err);
+      }
+    },
   },
 };
 
