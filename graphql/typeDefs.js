5851e307ebb0a2762f4f0dcb4310da275da94229
Konstantin Krumin <konstantin.krumin@gmail.com>
2021-11-23 16:08:13 +0300

    set up adding and deleting comments on the posts

--- graphql/typeDefs.js
+++ graphql/typeDefs.js
@@ -6,6 +6,21 @@ const typeDefs = gql`
     body: String!
     createdAt: String!
     username: String!
+    comments: [Comment]!
+    likes: [Like]!
+  }
+
+  type Comment {
+    id: ID!
+    createdAt: String!
+    username: String!
+    body: String!
+  }
+
+  type Like {
+    id: ID!
+    createdAt: String!
+    username: String!
   }
 
   type User {
@@ -32,7 +47,10 @@ const typeDefs = gql`
     register(registerInput: RegisterInput): User!
     login(username: String!, password: String!): User!
     createPost(body: String!): Post!
-    deletePost(postId: String!): String!
+    deletePost(postId: ID!): String!
+    createComment(postId: ID!, body: String!): Post!
+    deleteComment(postId: ID!, commentId: ID!): Post!
+    likePost(postId: ID!): Post!
   }
 `;
 
