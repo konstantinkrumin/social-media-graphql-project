3851721f0ece18d4e8344c9e1b5b7d86c2d88cde
Konstantin Krumin <konstantin.krumin@gmail.com>
2021-11-24 19:08:10 +0300

    set up createPost functionality and passing of the token in the Authorization header

--- client/src/ApolloProvider.js
+++ client/src/ApolloProvider.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { ApolloClient, ApolloProvider, createHttpLink, InMemoryCache } from '@apollo/client';
+import { setContext } from 'apollo-link-context';
 
 import App from './App';
 
@@ -7,8 +8,17 @@ const httpLink = createHttpLink({
   uri: 'http://localhost:5000',
 });
 
+const authLink = setContext(() => {
+  const token = localStorage.getItem('jwtToken');
+  return {
+    headers: {
+      Authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
+
 const client = new ApolloClient({
-  link: httpLink,
+  link: authLink.concat(httpLink),
   cache: new InMemoryCache(),
 });
 
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,11 +1,14 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Grid } from 'semantic-ui-react';
 import { useQuery } from '@apollo/client';
 import gql from 'graphql-tag';
 
+import { AuthContext } from '../context/auth';
 import PostCard from '../components/PostCard';
+import PostForm from '../components/PostForm';
 
 const Home = () => {
+  const { user } = useContext(AuthContext);
   const { loading, data } = useQuery(FETCH_POSTS_QUERY);
 
   return (
@@ -14,6 +17,11 @@ const Home = () => {
         <h1>Recent Posts</h1>
       </Grid.Row>
       <Grid.Row>
+        {user && (
+          <Grid.Column>
+            <PostForm />
+          </Grid.Column>
+        )}
         {loading ? (
           <h1>Loading posts...</h1>
         ) : (
