4473e215f25d78af0eb4303761c86554692eea46
Konstantin Krumin <konstantin.krumin@gmail.com>
2021-11-24 12:50:28 +0300

    changed the version of the react-router-dom

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import { Container } from 'semantic-ui-react';
 
 import MenuBar from './components/MenuBar';
@@ -15,11 +15,9 @@ function App() {
     <Router>
       <Container>
         <MenuBar />
-        <Routes>
-          <Route exact path="/" component={Home} />
-          <Route exact path="/login" component={Login} />
-          <Route exact path="/register" component={Register} />
-        </Routes>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/login" component={Login} />
+        <Route exact path="/register" component={Register} />
       </Container>
     </Router>
   );
